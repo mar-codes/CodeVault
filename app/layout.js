fa5ad1e47195a1fc3fd97614f6549d980dcb3bec
ghowsting.dev <131543691+mar-codes@users.noreply.github.com>
2025-03-10 07:13:31 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,28 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata = {
   title: 'CodeVault - Save Your Code',
   description: 'Platform to save and share code snippets',
+  openGraph: {
+    title: 'CodeVault - Save Your Code',
+    description: 'Platform to save and share code snippets',
+    url: 'https://codevault.lol',
+    siteName: 'CodeVault',
+    images: [
+      {
+        url: '/codevault.svg',
+        width: 1200,
+        height: 630,
+        alt: 'CodeVault Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CodeVault - Save Your Code',
+    description: 'Platform to save and share code snippets',
+    images: ['/codevault.svg'],
+  },
 }
 
 export default function RootLayout({ children }) {
