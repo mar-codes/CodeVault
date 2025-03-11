b294ab88d4b7a9dbf38cff4b6633ede80b7b5d34
ghowsting.dev <131543691+mar-codes@users.noreply.github.com>
2025-03-11 18:21:51 -0500

    Delete app directory

--- app/auth/user/profile/view/creations/[userId]/page.js
+++
@@ -1,7 +0,0 @@
-import UserCreationsContent from '@/app/components/UserCreations';
-
-
-export default async function UserCreationsPage({ params }) {
-  const { userId } = await params;
-  return <UserCreationsContent userId={userId} />;
-}
