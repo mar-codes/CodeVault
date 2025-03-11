edfb2470616b32f642597d239e8f4474b542fecf
ghowsting.dev <131543691+mar-codes@users.noreply.github.com>
2025-03-11 18:13:45 -0500

    Delete app directory

--- app/auth/user/profile/view/[userId]/page.js
+++
@@ -1,6 +0,0 @@
-import UserProfileContent from '@/app/components/UserProfile';
-
-export default async function UserProfile({ params }) {
-    const { userId } = await params;
-    return <UserProfileContent userId={userId} />;
-}
--- app/utils/Debounce.js
+++
@@ -1,7 +0,0 @@
-export default function debounce(fn, ms) {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => fn(...args), ms);
-    };
-}
